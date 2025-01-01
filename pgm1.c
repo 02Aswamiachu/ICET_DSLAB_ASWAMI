920f6f721cae55a2730fe578ea84023e35eb0478
02Aswamiachu <aswamiachu116@gmail.com>
2025-01-01 12:49:44 +0530

    S1 DS PROGRAMS

---
+++ pgm1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    int a, b, sum = 0;
+  
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+   
+    sum = a + b;
+
+    printf("Sum: %d", sum);
+
+   
+}
